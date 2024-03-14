5798553c8d4c8d929c64b0573b83ba03b784cb70
tej0897 <tej897@gmail.com>
2024-03-14 12:59:30 +0530

    Using Dependency Injection

--- src/main/java/com/spring/core/DependencyInjection/MessageSender.java
+++ src/main/java/com/spring/core/DependencyInjection/MessageSender.java
@@ -1,10 +1,16 @@
 package com.spring.core.DependencyInjection;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.stereotype.Component;
+
+@Component
 public class MessageSender {
 
     private final MessageService messageService;
 
-    public MessageSender(MessageService messageService){
+    @Autowired
+    public MessageSender(@Qualifier("emailService") MessageService messageService){
         this.messageService = messageService;
     }
 
