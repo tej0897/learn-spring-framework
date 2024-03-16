d2669249decbec1249c4af38ca2d0b7b0d784a6e
tej0897 <tej897@gmail.com>
2024-03-16 17:51:42 +0530

    beans commit-2

--- src/main/java/com/spring/core/Beans/BeanAnnotationDemo.java
+++ src/main/java/com/spring/core/Beans/BeanAnnotationDemo.java
@@ -14,13 +14,14 @@ class Student{
     }
 
     public void print(){
-        System.out.println("Student class called...");
+        System.out.println("Student class method called...");
+        address.print();
     }
 }
 
 class Address{
     public void print(){
-        System.out.println("Address class called...");
+        System.out.println("Address class method called...");
     }
 }
 
@@ -41,10 +42,21 @@ class AppConfig{
 public class BeanAnnotationDemo {
     public static void main(String[] args) {
         ApplicationContext applicationContext = new AnnotationConfigApplicationContext(AppConfig.class);
-        Student student = applicationContext.getBean(Student.class);
-        Address address = applicationContext.getBean(Address.class);
+        //Student student = applicationContext.getBean(Student.class);
+
+        //setting application context using bean name instead of class
+        Student student = (Student) applicationContext.getBean("student");
+
         student.print();
-        address.print();
+
+        // to get bean names:
+
+        String [] beanNames = applicationContext.getBeanDefinitionNames();
+        for (String bean : beanNames){
+            System.out.println(bean);
+        }
+
+
     }
 
 }
